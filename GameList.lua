1ddf49d22fda6b31958d03f7d4b9465dd150b231
Zyro999 <154056739+GalaxySR@users.noreply.github.com>
2025-03-11 22:59:33 +1100

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,6 @@
 local Games = {
     [155615604] = 'https://raw.githubusercontent.com/GalaxySR/Script-Games/refs/heads/main/Prison%20Life.lua', -- Prison Life
-    [9865958871] = '
+    [9865958871] = 'https://raw.githubusercontent.com/GalaxySR/Script-Games/refs/heads/main/BIG%20Paintball%202.lua' -- BIG Paintball 2
   }
   
   return Games
