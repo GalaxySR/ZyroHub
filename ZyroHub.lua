6d8eaeb9b0256405e6fe1acfdeba0eeb420aaadf
Zyro999 <154056739+GalaxySR@users.noreply.github.com>
2025-03-11 19:08:57 +1100

    Update ZyroHub.lua

--- ZyroHub.lua
+++ ZyroHub.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/GalaxySR/ZyroHub/refs/heads/main/GameList.lua?token=GHSAT0AAAAAADAIMEKLNYZL6ZEANMIK2JEKZ6P4J5Q"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/GalaxySR/ZyroHub/refs/heads/main/GameList.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
